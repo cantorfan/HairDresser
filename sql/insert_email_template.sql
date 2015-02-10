ed1cc7830768d4eb8940e238a62957864b5916d5
tuxming <tuxming@gmail.com>
2015-02-10 16:13:54 +0800

    update sql.

--- sql/insert_email_template.sql
+++ sql/insert_email_template.sql
@@ -1,9 +1,9 @@
 --backup
-INSERT INTO `emailstemplate` VALUES (1, 1, 0, 'Hello, {customer}!\r\nThank you for registering at https://isalon2you-soft.com/manhattanonline/\r\n-------------------\r\nUsername: {login}\r\nPassword: {password}\r\n-------------------\r\n\r\nSincerely, Administration https://isalon2you-soft.com/manhattanonline/', '');
-INSERT INTO `emailstemplate` VALUES (2, 1, 1, 'Hello, {customer}!\r\n\r\nYou have an appointment at https://isalon2you-soft.com/manhattanonline/\r\n-------------------\r\nOperator: {operator}\r\nService: {service}\r\nDate: {date}\r\nTime: {time}\r\nShopping: {product}\r\n-------------------\r\n\r\nSincerely, Administration https://isalon2you-soft.com/manhattanonline/', '');
-INSERT INTO `emailstemplate` VALUES (3, 1, 2, 'Hello, {customer}!\r\nYou booking has been accepted.\r\n-------------------\r\nOperator: {operator}\r\nService: {service}\r\nDate: {date}\r\nTime: {time}\r\nShopping: {product}\r\n-------------------\r\n\r\nSincerely, Administration https://isalon2you-soft.com/manhattanonline/', '');
-INSERT INTO `emailstemplate` VALUES (4, 1, 3, 'Hello, {customer}!\r\nWe can not accept your booking.\r\nPlease make your booking again.\r\nYou old booking:\r\n-------------------\r\nOperator: {operator}\r\nService: {service}\r\nDate: {date}\r\nTime: {time}\r\nShopping: {product}\r\n-------------------\r\n\r\nSincerely, Administration https://isalon2you-soft.com/manhattanonline/', '');
-INSERT INTO `emailstemplate` VALUES (5, 1, 4, 'Hello, {customer}!\r\nForgot Username and Password.\r\n-------------------\r\nUsername: {login}\r\nPassword: {password}\r\n-------------------\r\n\r\nSincerely, Administration https://isalon2you-soft.com/manhattanonline/', '');
+--INSERT INTO `emailstemplate` VALUES (1, 1, 0, 'Hello, {customer}!\r\nThank you for registering at https://isalon2you-soft.com/manhattanonline/\r\n-------------------\r\nUsername: {login}\r\nPassword: {password}\r\n-------------------\r\n\r\nSincerely, Administration https://isalon2you-soft.com/manhattanonline/', '');
+--INSERT INTO `emailstemplate` VALUES (2, 1, 1, 'Hello, {customer}!\r\n\r\nYou have an appointment at https://isalon2you-soft.com/manhattanonline/\r\n-------------------\r\nOperator: {operator}\r\nService: {service}\r\nDate: {date}\r\nTime: {time}\r\nShopping: {product}\r\n-------------------\r\n\r\nSincerely, Administration https://isalon2you-soft.com/manhattanonline/', '');
+--INSERT INTO `emailstemplate` VALUES (3, 1, 2, 'Dear {customerName}, \r\n\r\nYou booking has been accepted.\r\nEmployee: {employee}\r\nService: {service}\r\nDate: {dateTime}\r\n\r\nSincerely, Administration https://isalon2you-soft.com/manhattanonline/', '');
+--INSERT INTO `emailstemplate` VALUES (4, 1, 3, 'Hello, {customer}!\r\nWe can not accept your booking.\r\nPlease make your booking again.\r\nYou old booking:\r\n-------------------\r\nOperator: {operator}\r\nService: {service}\r\nDate: {date}\r\nTime: {time}\r\nShopping: {product}\r\n-------------------\r\n\r\nSincerely, Administration https://isalon2you-soft.com/manhattanonline/', '');
+--INSERT INTO `emailstemplate` VALUES (5, 1, 4, 'Hello, {customer}!\r\nForgot Username and Password.\r\n-------------------\r\nUsername: {login}\r\nPassword: {password}\r\n-------------------\r\n\r\nSincerely, Administration https://isalon2you-soft.com/manhattanonline/', '');
 
 INSERT INTO `emailstemplate` VALUES (6, 1, 100, 'Dear {customerName},\r\n This is a reminder of the appointment at {appointmentTime}, Please enjoy time!', '');
 
@@ -15,7 +15,7 @@ INSERT INTO `emailstemplate` VALUES (8, 1, 102, 'Dear {customerName},\nThanks fo
 --update emailstemplate set text='<h3>Dear {customerName}</h3><h4>You booking has been accepted.</h4><table border="1"><tr><th>Employee</th><th>Service</th><th>Date</th></tr>${employee}{service}{dateTime}$</table><p>Sincerely, Administration https://isalon2you-soft.com/manhattanonline/</p>' where type=2;
 --update emailstemplate set text='Dear {customerName},\nThanks for using iSalon. Hope you enjoy the time with us.\n\n{dateTime}\nIsalon\n' where type=102;
 
---update emailstemplate set text='Dear {customerName}, \r\n\r\nYou booking has been accepted.\r\nEmployee: {employee}\r\nService: {service}\r\nDate: {dateTime}\r\n\r\nSincerely, Administration https://isalon2you-soft.com/manhattanonline/' where type=2;
+update emailstemplate set text='Dear {customerName}, \r\n\r\nYou booking has been accepted.\r\nEmployee: {employee}\r\nService: {service}\r\nDate: {dateTime}\r\n\r\nSincerely, Administration https://isalon2you-soft.com/manhattanonline/' where type=2;
 
 
 alter table `appointment` add column `is_send_appointment_mail` boolean default false; 
